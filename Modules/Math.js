798feb37c1af18a7ce58ce47a2d55a022886a505
Ayush Raj <ayushrajraj12v@gmail.com>
2026-09-21 23:07:51 +0530

    Types of Modules

--- Modules/Math.js
+++ Modules/Math.js
@@ -1,7 +1,25 @@
-function add(a,b){
-    return a+b;
+//------>Commoon Js<----------
+//  function add(a,b){
+//      return a+b;
+//  }
+
+// function subtract(a,b){
+//     return a-b;
+// }
+
+// // 
+
+// module.exports={
+//     add:add,
+//     subtract:subtract,
+// }
+
+// exports.add=(a,b)=>a+b;
+// exports.subtract=(a,b)=>a-b;
+
+//ES6 Module js
+
+export default function add(a,b){
+    console.log(a+b);
 }
 
-function sub(a,b){
-    return a-b;
-}
---
+++ Modules/index.js
@@ -0,0 +1,6 @@
+import defaultFunc from './Math.js'
+
+defaultFunc(3,8);
+// const math=require('./Math.js')
+
+// console.log(math.add(9                      ,7))
