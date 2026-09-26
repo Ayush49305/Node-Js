718d8c8feb4c632f3fb1b2ff5ea4d8597740e56b
Ayush Raj <ayushrajraj12v@gmail.com>
2026-09-26 22:41:08 +0530

    Path module

---
+++ Path module/index.js
@@ -0,0 +1,6 @@
+const path = require('node:path')
+
+// let myPath = path.join('user','manas','resume.js')
+// let myAbsolutePath = path.resolve('user','manas','resume.js')
+// console.log(myPath)
+// console.log(myAbsolutePath)
